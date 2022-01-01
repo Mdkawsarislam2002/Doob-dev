9e850b2186af238bcb6dff05d9727ff303126f41
Md Kawsar Islam Yeasin <mdkawsarislam2002@gmail.com>
2022-01-02 04:27:26 +0600

    addead index.html

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  mode: "jit",
+  content: ["./public/**/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        mainColor: "rgb(255,57,70)",
+        secondColor: "rgb(49,69,132)",
+      },
+    },
+  },
+  plugins: [],
+};
